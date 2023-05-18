4a69e5667f27aab03552f9366dd2ebf4075aad12
BlaizeInferno <96913755+BlaizingInfernoDragon123@users.noreply.github.com>
2023-05-18 03:04:58 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,69 +1,33 @@
-function exeFunc(){
-    function setCookie(Default, value, options = {}) {
+const followBtn = document.querySelector(".follow-btn");
+const body = document.querySelector("body");
 
-        options = {
-          path: '',
-          // add other defaults here if necessary
-          ...options
-        };
-      
-        if (options.expires instanceof Date) {
-          options.expires = options.expires.toUTCString();
-        }
-      
-        let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-      
-        for (let optionKey in options) {
-          updatedCookie += "; " + optionKey;
-          let optionValue = options[optionKey];
-          if (optionValue !== true) {
-            updatedCookie += "=" + optionValue;
-          }
-        }
-      
-        document.cookie = updatedCookie;
-      }
-      
-      // Example of use:
-      setCookie('user', 'John', {secure: true, 'max-age': 36000});
-      
-      function deleteCookie(Default) {
-        setCookie(Default, "", {
-          'max-age': 0
-        })
-      }
-      
-      // returns the cookie with the given name,
-      // or undefined if not found
-      function getCookie(Default) {
-        let matches = document.cookie.match(new RegExp(
-          "(?:^|; )" + Default.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-        ));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-      }
-      
-    } 
-    document.getElementById('dis').style.color = 'transparent';
-    document.getElementById('dis1').style.color = 'transparent';
-    document.getElementById('dis2').style.color = 'transparent';
-    document.getElementById('dis3').style.color = 'transparent';
-    document.getElementById('img').style.display = 'none';
-    document.getElementById("btn1").style.display = 'none';
-    document.getElementById("btn2").style.display = 'none';
-
-    setTimeout(exe,5500);
-    function exe(){
-    document.getElementById("btn1").style.display = 'inline-block';
-    document.getElementById("btn2").style.display = 'inline-block';
-    document.getElementById('dis').style.color = 'black';
-    document.getElementById('list').className = 'feedback-list';
-    document.getElementById('dis1').style.color = 'black';
-    document.getElementById('list1').className = 'feedback-list';
-    document.getElementById('dis2').style.color = 'black';
-    document.getElementById('list2').className = 'feedback-list';
-    document.getElementById('img').style.display = 'block';
-    document.getElementById('dis3').style.color = 'black';
-    document.getElementById('list3').className = 'feedback-list';
-    document.getElementById('list4').className = 'feedback-list';
-    document.getElementById('btns').className = 'feedback-list';
+followBtn.onclick = function(){
+    followBtn.innerText = 'Following...';
+    alert("Thanks for following me , you will be notified of my latest projectes soon, However if you want to unfollow then just long press in the button again if you are in a mobile, and in a desktop just right click on this button");
+    const storage = localStorage.setItem("Follow","True");
+    console.log("Thanks for following me , you will be notified of my latest projectes soon, However if you want to unfollow then just long press in the button again");
+    console.log(storage);
+}
+body.onload = function(){
+    const item = localStorage.getItem("Follow");
+    if (item == 'True'){
+        followBtn.innerText = 'Following...';
     }
+    else{
+        followBtn.innerText = 'Follow Me';
+    }
+}
+followBtn.addEventListener('contextmenu',(e) =>{
+    e.preventDefault();
+    const enter = prompt("Are you sure you want to stop following? Please type in Yes or No to continue");
+    if(enter.value = 'Yes'){
+        localStorage.removeItem("Follow");
+        followBtn.innerText = 'Follow Me';
+        console.log("Unfollowed successfully!!")
+    }
+    else{
+        alert("Current Status : Following")
+        window.location.reload(true);
+    }
+    console.log(e);
+})
