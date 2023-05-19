d4381c5b120876fd1a3feeeb2b9500714c4d5757
BlaizeInferno <96913755+BlaizingInfernoDragon123@users.noreply.github.com>
2023-05-19 03:46:12 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const followBtn = document.querySelector(".follow-btn");
 const body = document.querySelector("body");
 
+/*document.addEventListener('contextmenu',(e) =>{
+    e.preventDefault();
+    alert("For Source Code Visit :- https://github.com/blaizinginfernodragon123/profile-card/")
+})*/
+
 followBtn.onclick = function(){
     followBtn.innerText = 'Following...';
     alert("Thanks for following me , you will be notified of my latest projectes soon, However if you want to unfollow then just long press in the button again if you are in a mobile, and in a desktop just right click on this button");
@@ -13,17 +18,20 @@ body.onload = function(){
     if (item == 'True'){
         followBtn.innerText = 'Following...';
     }
-    else{
-        followBtn.innerText = 'Follow Me';
-    }
+    //else{
+        //followBtn.innerText = 'Follow Me';
+    //}
 }
 followBtn.addEventListener('contextmenu',(e) =>{
     e.preventDefault();
-    const enter = prompt("Are you sure you want to stop following? Please type in Yes or No to continue");
+     enter = prompt("Are you sure you want to stop following? Please type in Yes or No to continue");
     if(enter.value = 'Yes'){
         localStorage.removeItem("Follow");
         followBtn.innerText = 'Follow Me';
-        console.log("Unfollowed successfully!!")
+        console.log("Unfollowed successfully!!");
+    }
+    if (enter.value = 'No'){
+        alert("Wish granted !!, you are still following me :)");
     }
     else{
         alert("Current Status : Following")
